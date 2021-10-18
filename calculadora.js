ab65b7f2c43c04657d9049f8bf79a4f8f4b732b6
Ivana-Benitez <ivana_estudiantes@hotmail.com>
2021-10-18 08:07:02 -0300

    subiendo

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+const sumar = require("./sumar");
+const restar = require("./restar");
+const multiplicar = require("./multiplicar");
+const dividir = require("./dividir");
+
+console.log(sumar(5,"a"));
+console.log(restar(5,2));
+console.log(multiplicar(5,2));
+console.log(dividir(5,2));
